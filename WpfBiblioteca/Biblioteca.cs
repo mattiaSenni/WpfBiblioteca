8df005f56333bd050f8f41a8a510c459302f30b1
giacomolazzari <giacomo.lazzari.stud@ispascalcomandini.it>
2020-11-18 10:59:00 +0100

    Aggiunta e definizione delle proprietà e dei metodi  AddLibro e CercaLibroTitolo

--- WpfBiblioteca/Biblioteca.cs
+++ WpfBiblioteca/Biblioteca.cs
@@ -2,24 +2,123 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WpfBiblioteca
 {
-    class Biblioteca
+    public class Biblioteca
     {
         private string _nome;
         private string _indirizzo;
         private string _orarioApertura;
         private string _orarioChiusura;
         private List<Libro> _listaLibri;
-        public Biblioteca(string nome,string indirizzo,string orarioApertura,string orarioChiusura,List<Libro> listaLibri)
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura, List<Libro> listaLibri)
         {
-            _nome = nome;
-            _indirizzo = indirizzo;
-            _orarioApertura = orarioApertura;
-            _orarioChiusura = orarioChiusura;
-            _listaLibri = listaLibri;   
+            Nome = nome;
+            Indirizzo = indirizzo;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+            ListaLibri = listaLibri;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Nome non valido");
+                }
+                else
+                    _nome = value;
+            }
+        }
+
+        public string Indirizzo
+        {
+            get
+            {
+                return _indirizzo;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Indirizzo non valido");
+                }
+                else
+                    _indirizzo = value;
+            }
+        }
+
+        public string OrarioApertura
+        {
+            get
+            {
+                return _orarioApertura;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Orario di apertura non valido");
+                }
+                else
+                    _orarioApertura = value;
+            }
+        }
+
+        public string OrarioChiusura
+        {
+            get
+            {
+                return _orarioChiusura;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Orario di chiusura non valido");
+                }
+                else
+                    _orarioChiusura = value;
+            }
+        }
+
+        public List<Libro> ListaLibri
+        {
+            get;
+            set;     
+        }
+
+        public void AddLibro(Libro libro)
+        {
+            _listaLibri.Add(libro);
+        }
+
+        public Libro CercaLibroTitolo(string titolo)
+        {
+            foreach(Libro l in _listaLibri)
+            {
+                if(l.Titolo == titolo)
+                {
+                    return l;
+                }
+            }
+        }
+
+        public void CercaLibriAutore(string nominativoAutore)
+        {
+           
+        }
+
+        public void Numerolibri()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
