c4363059c6c1aa38e66044efa6cba63b966322e0
diego marcos.poli <diego marcos.poli@PC7317>
2020-11-18 10:52:21 +0100

    implementazione proprietà

--- WpfBiblioteca/Libro.cs
+++ WpfBiblioteca/Libro.cs
@@ -18,7 +18,81 @@ namespace WpfBiblioteca
         {
 
         }
-
+        public string Titolo
+        {
+            get
+            {
+                return _titolo;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Serve un titolo");
+                }
+                _titolo = value;
+            }
+        }
+        public string Autore
+        {
+            get
+            {
+                return _autore;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Serve un Autore");
+                }
+                _autore = value;
+            }
+        }
+        public string Editore
+        {
+            get
+            {
+                return _editore;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Serve un Editore");
+                }
+                _editore = value;
+            }
+        }
+        public int AnnoPubblicazione
+        {
+            get
+            {
+                return _annoPubblicazione;
+            }
+            set
+            {
+                if (value<-3000)
+                {
+                    throw new Exception("Anno non valido");
+                }
+                _annoPubblicazione = value;
+            }
+        }
+        public int NumeroPag
+        {
+            get
+            {
+                return _numeroPag;
+            }
+            set
+            {
+                if (value <=0)
+                {
+                    throw new Exception("Il libro deve avere delle pagine");
+                }
+                _numeroPag = value;
+            }
+        }
 
     }
 }
