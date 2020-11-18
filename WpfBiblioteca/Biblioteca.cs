9fe4dad6287c9297420a41980f46a76292eb1719
giacomolazzari <giacomo.lazzari.stud@ispascalcomandini.it>
2020-11-18 10:43:46 +0100

    Aggiunta costruttore e attributi

---
+++ WpfBiblioteca/Biblioteca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBiblioteca
+{
+    class Biblioteca
+    {
+        private string _nome;
+        private string _indirizzo;
+        private string _orarioApertura;
+        private string _orarioChiusura;
+        private List<Libro> _listaLibri;
+        public Biblioteca(string nome,string indirizzo,string orarioApertura,string orarioChiusura,List<Libro> listaLibri)
+        {
+            _nome = nome;
+            _indirizzo = indirizzo;
+            _orarioApertura = orarioApertura;
+            _orarioChiusura = orarioChiusura;
+            _listaLibri = listaLibri;   
+        }
+    }
+}
