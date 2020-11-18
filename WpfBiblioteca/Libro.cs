f0e974ada91bb9b5cf6cf796ea461131ba7b5b31
diego marcos.poli <diego marcos.poli@PC7317>
2020-11-18 10:44:46 +0100

    creazione Classe Libro con metodo costruttore e campi

---
+++ WpfBiblioteca/Libro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBiblioteca
+{
+    class Libro
+    {
+        private string _titolo;
+        private string _autore;
+        private int _annoPubblicazione;
+        private string _editore;
+        private int _numeroPag;
+
+        public Libro(string titolo,string autore,int annoPubblicazione,string editore,int numeroPag )
+        {
+
+        }
+
+
+    }
+}
